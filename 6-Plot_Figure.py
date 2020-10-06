50b231a3748ecb7a072db06d79a5af8bcd260a3b
SonghuaHu-UMD <hsonghua@UMD.EDU>
2020-10-06 18:01:06 -0400

    1006

--- 6-Plot_Figure.py
+++
@@ -1,4 +0,0 @@
-# What should we plot
-# Figure 1: Total Temporal time-series and Number of cases
-# Figure 2: A BSTS Example plot
-# Figure 3: Spatial Plot
--- [N]4-Model_Build.R
+++ [N]4-Model_Build.R
@@ -55,12 +55,11 @@ for (var in Need_Loop){
   ggsave(paste(var,"_Single.svg"), units="in", width=7, height=5)
 }
 
-# Cross Estimation
+# Cross-Section Estimation
 # Find the last row
 dat$from_stati <- as.numeric(dat$from_stati)
 dat_avg <- dat %>% group_by(from_stati) %>% slice(c(n())) %>% ungroup()
-
-
+# 2019 Average Pickup Model
 GAM_RES1 <- mgcv::gam(X2019_Avg ~  Prop.of.Male + Prop.of.Age_25_40  + Prop.of.White + Prop.of.Asian + Median.Income +
   Prop.of.College.Degree + Prop.of.Utilities.Jobs + Prop.of.Goods_Product.Jobs + Population.Density + Job.Density +
   Prop.of.Car + Prop.of.Transit +
@@ -68,7 +67,7 @@ GAM_RES1 <- mgcv::gam(X2019_Avg ~  Prop.of.Male + Prop.of.Age_25_40  + Prop.of.W
   Road.Density + Bike.Route.Density  + Transit.Ridership  + Distance.to.Nearest.Bike.Station +  Distance.to.City.Center +
   Capacity + ti(lat, lon), data = dat_avg, family = c("nb"), method = "REML")
 summary(GAM_RES1)
-
+# The Accumulative Relative Drop Model
 GAM_RES2 <- mgcv::gam(Cum_Relative_Impact ~  Prop.of.Male + Prop.of.Age_25_40  + Prop.of.White + Prop.of.Asian + Median.Income +
   Prop.of.College.Degree + Prop.of.Utilities.Jobs + Prop.of.Goods_Product.Jobs + Population.Density + Job.Density +
   Prop.of.Car + Prop.of.Transit +
@@ -76,7 +75,7 @@ GAM_RES2 <- mgcv::gam(Cum_Relative_Impact ~  Prop.of.Male + Prop.of.Age_25_40  +
   Road.Density + Bike.Route.Density  + Transit.Ridership  + Distance.to.Nearest.Bike.Station +  Distance.to.City.Center +
   Capacity + No.of.Cases + Infection.Rate + ti(lat, lon), data = dat_avg, family = c("gaussian"), method = "REML")
 summary(GAM_RES2)
-
+# Start to Drop Date Model
 dat_start <- dat[dat$Date == '2020-03-10',]
 GAM_RES3 <- mgcv::gam(Cum_Relative_Impact ~  Prop.of.Male + Prop.of.Age_25_40  + Prop.of.White + Prop.of.Asian + Median.Income +
   Prop.of.College.Degree + Prop.of.Utilities.Jobs + Prop.of.Goods_Product.Jobs + Population.Density + Job.Density +
@@ -108,7 +107,7 @@ vif_test <-
 vif(vif_test)
 summary(vif_test)
 
-# Stepwise select variables
+# Stepwise: Help to select variables
 steps <- step(lm(Cum_Relative_Impact ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Asian +
        Income + College +  Cumu_Cases_Rate + Cumu_Cases  + COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE +
   Bike_Route + Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density +  alightings   +
@@ -124,60 +123,21 @@ GAM_RES1 <- mgcv::gam(Cum_Relative_Impact ~  Pct.Male + Pct.Age_0_24 + Pct.Age_2
     ti(lat, lon),data = dat_avg, family = c("gaussian"), method = "REML", )
 summary(GAM_RES1)
 
-# VIF TEST
-vif_test <-
-  lm(X2019_Avg ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Black + Pct.Asian +
-       Income + College +  Cumu_Cases_Rate + Cumu_Cases + Cumu_Death+Cumu_Death_Rate+COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE + RESIDENTIAL +
-    Bike_Route + Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density +  alightings  + Distance_Busstop +
-    rides + Distance_Rail  + Near_Bike_Capacity + Distance_Bikestation +  Distance_City + PopDensity + capacity,
-     data = dat_avg)
-vif(vif_test)
-summary(vif_test)
-
-# Stepwise select variables
-steps <- step(lm(X2019_Avg ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Asian +
-       Income + College + COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE +
-  Bike_Route + Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density +  alightings   +
-  rides + Distance_Rail  + Near_Bike_Capacity + Distance_Bikestation +  Distance_City + PopDensity + capacity,
-     data = dat_avg))
-summary(steps)
-
-GAM_RES1 <- mgcv::gam(X2019_Avg ~  Pct.Male + Pct.Age_25_40  + Pct.White + Pct.Asian +
-       Income + College + COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE +
-  Bike_Route + Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density    +
-  transit   + Near_Bike_Capacity + Distance_Bikestation +  Distance_City + PopDensity + capacity+
-    ti(lat, lon),data = dat_avg, family = c("nb"), method = "REML", )
-summary(GAM_RES1)
-
-GAM_RES1 <-
-  mgcv::gam(Response ~  Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Black + Pct.Asian +
-       Income + College + Pct.Car + Pct.BikeWalk + Pct.WorkHome +  Cumu_Death +
-    COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE + RESIDENTIAL + Bike_Route +
-    Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density + Bus_stop_count + boardings  +
-    Distance_Busstop + Rail_stop_count + rides + Distance_Rail  + Near_Bike_Capacity +
-    Distance_Bikestation + Near_bike_pickups + Distance_City + PopDensity + capacity+
-    ti(lat, lon),data = dat_avg, family = c("gaussian"), method = "REML", )
-summary(GAM_RES1)
-
-colnames(dat)
-# Time_varying
+# Time_varying model
 GAM_RES2 <-
-  mgcv::bam(Cum_Relative_Impact ~
-              Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Asian +
-       Income + College +  Cumu_Cases +
-    COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE  + Bike_Route +
+  mgcv::bam(Cum_Relative_Impact ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Asian +
+    Income + College +  Cumu_Cases + COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE  + Bike_Route +
     Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density +  alightings  +
     Distance_Busstop +  rides + Distance_Rail  + Near_Bike_Capacity +
     Distance_Bikestation +  Distance_City + PopDensity + capacity + TMAX + PRCP +
-                ti(lat, lon) + s(Week,k=5) + s(Month,k=5) + s(from_stati,bs = 're') + s(Time_Index),
+    ti(lat, lon) + s(Week,k=5) + s(Month,k=5) + s(from_stati,bs = 're') + s(Time_Index),
             data = dat, family = c("gaussian"), control = gam.control(trace = TRUE),
             method = "fREML", discrete = TRUE, chunk.size=5000, nthreads = 150)
 summary(GAM_RES2)
 plot(GAM_RES2)
 
 GAM_RES2 <-
-  mgcv::bam(Response ~
-              Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Black + Pct.Asian +
+  mgcv::bam(Response ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White + Pct.Black + Pct.Asian +
                 Income + College  + Cumu_Cases + Cumu_Death + COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE +
                 RESIDENTIAL +  Bike_Route + Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density +
                 alightings   + rides   + Distance_Bikestation + Distance_City + PopDensity + capacity + TMAX_2020 + PRCP_2020 +
@@ -186,41 +146,3 @@ GAM_RES2 <-
             method = "fREML", discrete = TRUE, chunk.size=5000, nthreads = 150)
 summary(GAM_RES2)
 plot(GAM_RES2)
-
-# How about the average
-#dat_avg <- aggregate(dat, list(dat$from_stati), mean,na.rm = TRUE)
-dat_avg <- dat <- read.csv('D:\\COVID19-Transit_Bikesharing\\Divvy_Data\\Avg_final_Divvy_R_0907.csv')
-dat_avg$Pct.BikeWalk <- dat_avg$Pct.Bicycle + dat_avg$Pct.Walk
-
-vif_test <-
-  lm(Relative_Impact ~ Pct.Male + Pct.Age_0_24 + Pct.Age_25_40 + Pct.Age_40_65 + Pct.White  + Pct.Asian +
-       Income + College + Pct.Car + Pct.BikeWalk + Pct.WorkHome + Cumu_Cases + Cumu_Death +
-    COMMERCIAL + INDUSTRIAL + INSTITUTIONAL + OPENSPACE + RESIDENTIAL + Primary + Secondary + Minor + Bike_Route +
-    Pct.WJob_Utilities + Pct.WJob_Goods_Product + WTotal_Job_Density  + boardings  +
-    Distance_Busstop  + rides + Distance_Rail  +
-    Distance_Bikestation + Near_bike_pickups + Distance_City + PopDensity + capacity,
-     data = dat_avg
-  )
-vif(vif_test)
-summary(vif_test)
-
-
-# LASSO
-x <- dat_avg %>%
-  dplyr::select(Pct.Male , Pct.Age_0_24 , Pct.Age_25_40 , Pct.Age_40_65 , Pct.White , Pct.Black , Pct.Asian ,
-       Income , College , Pct.Car , Pct.BikeWalk , Pct.WorkHome , Cumu_Cases , Cumu_Death ,
-    COMMERCIAL , INDUSTRIAL , INSTITUTIONAL , OPENSPACE , RESIDENTIAL , Primary , Secondary , Minor , Bike_Route ,
-    Pct.WJob_Utilities , Pct.WJob_Goods_Product , WTotal_Job_Density , Bus_stop_count , boardings  ,
-    Distance_Busstop , Rail_stop_count , rides , Distance_Rail  , Near_Bike_Capacity ,
-    Distance_Bikestation , Near_bike_pickups , Distance_City , PopDensity , capacity) %>%
-  data.matrix()
-## For impact
-y <- dat_avg$Cum_Relt_Effect
-lambdas <- 10^seq(5, -5, by = -.1)
-cv_fit <- cv.glmnet(x, y, alpha = 0, lambda = lambdas)
-plot(cv_fit)
-opt_lambda <- cv_fit$lambda.min
-# Rebuilding the model with optimal lambda value
-best_lasso <- glmnet(x, y, alpha = 0, lambda = cv_fit$lambda.min)
-coef(best_lasso)
-
